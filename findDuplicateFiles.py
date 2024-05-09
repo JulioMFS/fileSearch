71bd678492f30da33fc420fc4161dcb50dc9ea65
JulioMFS <75912060+JulioMFS@users.noreply.github.com>
2024-05-09 14:49:08 +0100

    Coiunt Extensions

--- findDuplicateFiles.py
+++ findDuplicateFiles.py
@@ -4,6 +4,7 @@ import sys
 import timeit
 from collections import defaultdict
 from datetime import datetime
+from colorist import Color
 
 def get_file_hash(file_path):
     hasher = hashlib.sha256()
@@ -24,17 +25,27 @@ def get_file_hash(file_path):
         print(f"An unexpected error occurred: {e}")
     return hasher.hexdigest()
 
-def find_duplicate_files(directory):
+def find_duplicate_files(directory, log_file):
     file_hash_map = defaultdict(list)
     extensions = {}
+    totsz = 0
+    totSzToDelete = 0
     for root, _, files in os.walk(directory):
         for filename in files:
             file_path = os.path.join(root, filename)
             f, ext = os.path.splitext(file_path)
+            sz = os.path.getsize(file_path)
+            val = [0, 0]
             if ext in extensions:
-                extensions[ext] += 1
+                val = extensions[ext]
+                val[0] += 1
+                val[1] += sz
+                extensions[ext] = val
             else:
-                extensions[ext] = 1
+                val[0] = 1
+                val[1] = sz
+                extensions[ext] = val
+            totsz += sz
             if 'eclipse' in file_path or 'Program Files' in file_path:
                 continue
             file_hash = get_file_hash(file_path)
@@ -45,11 +56,21 @@ def find_duplicate_files(directory):
     for file_hash, file_paths in file_hash_map.items():
         if len(file_paths) > 1:
             duplicate_files.extend(file_paths)
+            i = 0
             for file_path in file_paths:
                 f = file_path.split()
-                print('{:<30}{}'.format(f[0], f[1]))
+                sz = os.path.getsize(f[1])
+                txt = '{:<30}{:<60}{}'.format(f[0], f[1], sz)
+ #               print("\033[031m" + "Hello" + "\033[0m")
+                if i > 0:
+                    print(txt, file = log_file)
+                    print(f'{Color.RED} {txt} {Color.OFF}')
+                    totSzToDelete += sz
+                else:
+                    print(txt)
+                i += 1
             print()
-    return duplicate_files, extensions
+    return duplicate_files, extensions, totSzToDelete
 
 def printTime(msg, start, start_time, end_time):
     end = timeit.default_timer()
@@ -78,16 +99,17 @@ prog = tempTuple[0]
 logfile = prog + '.log'
 log_file = open(logfile,"w")
 
-sys.stdout = log_file
+#sys.stdout = log_file
 # Specify the directory to search for duplicate files
 sep = os.sep
-directory_to_search = "D:\AgromaisTest"
-
+directory_to_search = "D:"
+#directory_to_search = "D:\AgromaisTest"
 #directory_to_search = '/media/julio/TOSHIBA EXT'
 # Find duplicate files
-duplicates, extensions = find_duplicate_files(directory_to_search)
+duplicates, extensions, totsz = find_duplicate_files(directory_to_search, log_file)
 if duplicates:
     print("\nDuplicate files found by prog: {0}, in {1}\n".format(progWithExtension, directory_to_search))
+    print('Total bytes to delete: {}\n'.format(totsz))
     str = ''
     #for duplicate in duplicates:
     #    str1 = duplicate.split(' --> ')
@@ -98,8 +120,13 @@ if duplicates:
 else:
     print("No duplicate files found.")
 if extensions:
+    totsz = 0
     for k, v in extensions.items():
-        print(k, ' ', v)
+        val = v
+        print('{:<7}{:>10}{:>10}'.format(k, val[0], val[1]))
+        totsz += val[1]
+    print('{}{}'.format(' ' * 16, '-' * 11))
+    print('total ext bytes: {:>10}'.format(totsz))
 end_time = datetime.now()
 # Format the current time as a string
 end_time_str = end_time.strftime("%Y-%m-%d %H:%M:%S")
